64f99e15d4661c2386f2df0e18bf246bfd6945b1
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2022-04-01 09:41:10 +0500

    added solid projects

---
+++ InterfaceSegrigation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceSegrigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
